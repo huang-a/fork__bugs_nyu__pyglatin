301061bc10217cda06df4794426df1bbbc227384
Andy Huang <huang-a@users.noreply.github.com>
2019-10-08 13:52:27 -0400

    Fix english.py

--- english.py
+++ english.py
@@ -4,8 +4,8 @@ VOWELS = "aeiou"
 
 def to_piglatin(text):
     word_list = []
-    for word in whitespace.split(text):
-        word_list.append(_word_to_english(word))
+    for word in text.split():
+        word_list.append(_word_to_piglatin(word))
     return ' '.join(word_list)
 
 def _word_to_piglatin(word):
